fc8b303203df702fae6f053a569ebb55f3467a63
James Golick <james@giraffesoft.ca>
2008-05-31 12:15:49 -0400

    add start and stop methods for starting the server up

--- lib/scrawny.rb
+++ lib/scrawny.rb
@@ -1,9 +1,22 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rubygems'
 require 'activesupport'
+require 'activerecord'
 require 'thin'
 require File.dirname(__FILE__)+'/../vendor/invisible/lib/invisible'
 
 module Scrawny
-  
+  class << self
+    def start!
+      @server = Thin::Server.new '0.0.0.0', 5432 do
+        map('/') { run Invisible::Application.new }
+      end
+      
+      @server.start
+    end
+    
+    def stop!
+      @server.stop if @server
+    end
+  end
 end
