ad465ecf6d64e105d0d35bac52f25880bffef07d
James Golick <james@giraffesoft.ca>
2008-05-31 12:43:14 -0400

    create the queue_items table if it doesn't exist

--- lib/scrawny.rb
+++ lib/scrawny.rb
@@ -3,15 +3,13 @@ require 'rubygems'
 require 'activesupport'
 require 'activerecord'
 require 'thin'
+require 'scrawny/queue_item'
 require File.dirname(__FILE__)+'/../vendor/invisible/lib/invisible'
 
 module Scrawny
   class << self
     def start!
-      ActiveRecord::Base.establish_connection(
-        :adapter   => "sqlite3",
-        :database  => "queue.sqlite3"
-      )
+      establish_db_connection
       
       @server = Thin::Server.new '0.0.0.0', 5432 do
         map('/') { run Invisible::Application.new }
@@ -23,5 +21,16 @@ module Scrawny
     def stop!
       @server.stop if @server
     end
+    
+    protected
+      def establish_db_connection
+        ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "queue.sqlite3")
+        
+        unless QueueItem.table_exists?
+          ActiveRecord::Migration.create_table :queue_items do |t|
+            t.text :value
+          end
+        end
+      end
   end
 end
--- test/scrawny_test.rb
+++ test/scrawny_test.rb
@@ -1,5 +1,21 @@
 require File.dirname(__FILE__)+'/test_helper'
 
-class ScrawnyTest < Test::Unit::TestCase
+Expectations do
+  expect ActiveRecord::Base.to.receive(:establish_connection) do
+    Scrawny::QueueItem.stubs(:table_exists?).returns(true)
+    Thin::Server.any_instance.stubs(:start)
+    Scrawny.start!
+  end
   
+  expect ActiveRecord::Migration.to.receive(:create_table).with(:queue_items) do
+    ActiveRecord::Base.stubs(:establish_connection)
+    Scrawny::QueueItem.stubs(:table_exists?).returns(false)    
+    Scrawny.send(:establish_db_connection)
+  end
+  
+  expect ActiveRecord::Migration.to.receive(:create_table).times(0) do
+    ActiveRecord::Base.stubs(:establish_connection)
+    Scrawny::QueueItem.stubs(:table_exists?).returns(true)
+    Scrawny.send(:establish_db_connection)
+  end
 end
